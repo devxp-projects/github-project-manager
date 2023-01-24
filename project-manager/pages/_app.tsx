ff0cac4155d04513536965b43b5f90a41f845bb2
BrownCarter <lilbrown3000@gmail.com>
2023-01-24 03:19:59 +0100

    added: Created the Material theme provider

--- project-manager/pages/_app.tsx
+++ project-manager/pages/_app.tsx
@@ -1,18 +1,42 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useState } from 'react';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const [light, setLight] = useState(true);
+  const theme = createTheme({
+    palette: {
+      mode: light ? 'light' : 'dark',
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            color: light ? '#3765DC' : '#000',
+          },
+        },
+      },
+    },
+  });
+
+  const themeToggle = () => {
+    setLight((prev) => !prev);
+  };
+
   return (
     <>
-      <Head>
-        <title>Welcome to project-manager!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps}/>
-      </main>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Welcome to project-manager!</title>
+        </Head>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </>
   );
-};
+}
 
 export default CustomApp;
