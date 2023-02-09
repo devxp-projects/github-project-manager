785977fb9555c69b2e234944e1def863826356e0
vicson <vicsonabuka@gmail.com>
2023-02-09 07:27:10 +0100

    delete

--- movie-app/pages/Trial.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-export default function Trial ()  {
-return(
-    <div>
-    <Button variant="contained">Button</Button>
-    </div>
-)
-}
