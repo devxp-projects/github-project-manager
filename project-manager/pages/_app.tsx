a12a47660b43ed8bde532dc17a4f07dab55ebec5
vicson <vicsonabuka@gmail.com>
2023-02-09 07:22:43 +0100

    delete

--- project-manager/pages/_app.tsx
+++ project-manager/pages/_app.tsx
@@ -1,20 +1,23 @@
+// eslint-disable-line
+
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import useCustomTheme from '../utils/theme';
 import './styles.css';
 
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const [theme, themeToggle] = useCustomTheme();
   return (
     <>
       <Head>
-        <title>Welcome to project-manager</title>
+        <title>Welcome to project-manager!</title>
       </Head>
       <main className="app">
         <Component {...pageProps}/>
-  
       </main>
     </>
   );
-};
+}
 
 export default CustomApp;
