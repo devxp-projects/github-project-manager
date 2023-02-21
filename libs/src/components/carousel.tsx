202ef4abacd741026a3304b962e02205e42af8ae
vicson <vicsonabuka@gmail.com>
2023-02-21 17:40:39 +0100

    Add carousel component

---
+++ libs/src/components/carousel.tsx
@@ -0,0 +1,21 @@
+export default function Carousel() {
+  return (
+    <div className="container">
+      <div className="left">
+        <div className="parag1">
+          <p id="pa1">Movies</p>
+          <p id="pa2">532 Titles</p>
+        </div>
+      </div>
+      <img id="img1" src="/images/Movie11nobg 1.png" />
+
+      <div className="right">
+        <div className="parag">
+          <p id="p1">Anime</p>
+          <p id="p2">532 Titles</p>
+        </div>
+      </div>
+      <img id="img2" src="/images/Anime1nobg 1.png" />
+    </div>
+  );
+}
--- movie-app/pages/index.tsx
+++ movie-app/pages/index.tsx
@@ -1,39 +1,12 @@
  import styles from './index.module.scss';
-import React, {useState, useEffect} from 'react';
-import SplashScreen from '../../libs/src/components/splashscreen';
+import Carousel from '../../libs/src/components/carousel'
 
 export function Index() {
-  const [loading, setLoading]= useState(false);
-
-  useEffect(() =>{
-setLoading(true);
-
-  }, [])
   return (
-  
-    <div className={styles.page}>
-        
-    <div className="wrapper">
-        <div className='container'>
-       {
-        loading ?
-        <SplashScreen
-      
-        />
-        
-        :
-
-     <></>
-       }
-
-      
-
-  <footer className={styles.footer}>
-Copyright &copy; Victor Abuka
-  </footer>
+ <div className='container'>
+<Carousel/>
         </div>
-  </div>
-    </div>
+  
   );
 };
 
